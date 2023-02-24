908813e1a7e60f014c37e055fde5a7df6209827d
Sophie <61323326+sophiep99@users.noreply.github.com>
2023-02-24 10:18:24 -0500

    Bug fix favourite
    database reset
    made it so the author is set to the right author
    timestamp is set to the original timestamp

--- app/models.py
+++ app/models.py
@@ -300,7 +300,6 @@ class Favourite(SearchableMixin, db.Model):
     body = db.Column(db.String(140))
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-    language = db.Column(db.String(5))
-
+    current_id = db.Column(db.Integer)
     def __repr__(self):
         return '<Favourite {}>'.format(self.body)
