8a7c52172fb99aee979471fa9c6910ad6965d635
Bilal Sohail <bmsohail@hotmail.com>
2023-03-09 20:49:42 -0500

    negative login scenario

--- tests/features/environment.py
+++ tests/features/environment.py
@@ -4,15 +4,12 @@ from misc_methods import browser_helper
 def before_all(context):
     context.connected = {}
 
-
 def before_tag(context, tag):
     pass
 
-def before_feature(context, feature):
-    print('here 101')
-    time.sleep(10)
+def before_scenario(context, feature):
     context.driver = browser_helper.get_browser()
 
-def after_feature(context, feature):
+def after_scenario(context, feature):
     browser_helper.reset_options()
     context.driver.quit()
--- tests/features/steps/login.py
+++ tests/features/steps/login.py
@@ -36,7 +36,6 @@ def step_impl(context):
     assert previous_url != reusable_components.get_current_url(context)
 
 
-
 @then(u'I should see a welcome message with my username')
 def step_impl(context):
     text = reusable_components.get_text_of_an_element(context, [By.XPATH, '//h1'])
@@ -44,3 +43,14 @@ def step_impl(context):
         assert 'Hi'.lower() in text
     except:
         print('expected text not found in {}'.format(text))
+
+
+@when(u'I enter credentials {username} and {password}')
+def step_impl(context, username, password):
+    reusable_components.send_keys_to_element(context, login_locators.USERNAME_FIELD, username)
+    reusable_components.send_keys_to_element(context, login_locators.PASSWORD_FIELD, password)
+
+@then(u'I should see an error')
+def step_impl(context):
+    text = reusable_components.get_text_of_an_element(context, [By.CSS_SELECTOR, '[class="alert alert-info"]'])
+    assert text == "Invalid username or password"
